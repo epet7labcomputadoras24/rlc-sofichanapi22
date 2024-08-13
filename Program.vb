2b120d7aa1b761dd0e0c116126a32cf7ca203042
sofichanapi22 <chanapisofia22@gmail.com>
2024-08-13 20:04:06 -0300

    tarea

--- Program.vb
+++ Program.vb
@@ -1,7 +1,23 @@
-Imports System
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+Imports System.Net.NetworkInformatio
+Module module1
+    Sub Main()
+        Dim frecuencia As Double
+        Dim inductancia As Double
+        Dim capacitancia As Double
+        Dim resistencia As Double
+
+        Console.Write("ingrese la frecuencia")
+        frecuencia = Console.ReadLine()
+        Console.Write("ingrese la inductancia")
+        inductancia = Console.ReadLine()
+        Console.Write("ingrese la capacitancia")
+        capacitancia = Console.ReadLine()
+
+        Dim XL As Double = 2 * PI * frecuencia * inductancia
+        Dim XC As Double = 1 / (2 * PI * frecuencia * inductancia)
+        Dim R As Double = resistencia
+        Dim X As Double = XL - XC
+        Dim Z As Double = Math.Atan((XL - XC) / R)
     End Sub
 End Module
